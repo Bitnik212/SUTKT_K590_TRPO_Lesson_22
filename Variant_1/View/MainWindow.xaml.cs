477cd3ecdea153a70a33cd9c406a14acf1f2cf47
Николай Никоноров <nnv@nikolay.moe>
2021-10-02 02:10:07 +0300

    done/Вариант 1

--- Variant_1/View/MainWindow.xaml.cs
+++ Variant_1/View/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace Variant_1
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Variant_1.ViewModel;
+
+namespace Variant_1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
