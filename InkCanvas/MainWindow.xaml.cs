f4f3ca6bc96913c1fa429504dba85b6b7a34b260
Николай Никоноров <nnv@nikolay.moe>
2021-10-01 22:48:29 +0300

    done/Задание 8

--- InkCanvas/MainWindow.xaml.cs
+++ InkCanvas/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace InkCanvas
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+
+namespace InkCanvas
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +13,25 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void inkCanvas_Gesture(object sender, InkCanvasGestureEventArgs e)
+        {
+            String gestures = "";
+            // Выборка "предпологаемых" гестур.
+            foreach (GestureRecognitionResult res in
+                e.GetGestureRecognitionResults())
+                gestures += res.ApplicationGesture.ToString() + "; ";
+            // Отображаем название гестуры.
+            gestureName.Text = gestures;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Выборка всех режимов редактирования InkCanvas.
+            foreach (InkCanvasEditingMode mode in
+                Enum.GetValues(typeof(InkCanvasEditingMode)))
+                lstEditingMode.Items.Add(mode);
+            lstEditingMode.SelectedItem = inkCanvas.EditingMode;
+            
         }
     }
 }
