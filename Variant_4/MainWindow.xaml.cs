4bf3835d78e860661c989d7416809f9306002ce3
Николай Никоноров <nnv@nikolay.moe>
2021-10-02 10:32:47 +0300

    init/Вариант 4

---
+++ Variant_4/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+
+namespace Variant_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
