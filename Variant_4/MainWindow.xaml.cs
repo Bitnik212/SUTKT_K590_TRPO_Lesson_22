2dd1941f779099c90ce66a41d151bcf90af34b13
Николай Никоноров <nnv@nikolay.moe>
2021-10-02 10:33:10 +0300

    done/Вариант 4

--- Variant_4/MainWindow.xaml.cs
+++ Variant_4/MainWindow.xaml.cs
@@ -14,6 +14,22 @@ namespace Variant_4
         {
             InitializeComponent();
         }
-        
+        private void inkCanvas_Gesture(object sender, InkCanvasGestureEventArgs e)
+        {
+            String gestures = "";
+            // Выборка "предпологаемых" гестур.
+            foreach (GestureRecognitionResult res in
+                e.GetGestureRecognitionResults())
+                gestures += res.ApplicationGesture.ToString() + "; ";
+            // Отображаем название гестуры.
+            // gestureName.Text = gestures;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Выборка нужных режимов редактирования InkCanvas.
+            lstEditingMode.Items.Add(InkCanvasEditingMode.Ink);
+            lstEditingMode.Items.Add(InkCanvasEditingMode.EraseByPoint);
+            lstEditingMode.SelectedItem = inkCanvas.EditingMode;
+        }
     }
 }
